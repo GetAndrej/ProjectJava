0de68bda25a5e99707e42df2cced36cbef3c13b1
GetAndrej <andrej-zhadan@mail.ru>
2015-09-27 19:58:02 +0300

    Commit11

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Andrey");
-        System.out.print("Diam");
-        System.out.println("Дима у меня получилось))))");
-        System.out.println("fack");
+        System.out.println("Andrey o ld");
+
     }
 }
